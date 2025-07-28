c55b5f9fea8cc650cb8654b3775e5514f2cda46d
AlexAraldi <alexanderaraldi@gmail.com>
2025-07-28 14:18:27 -0300

    fix: editada as propriedades do arquivo compromisso.cs de acordo com as especificações necessárias para o metodo cadastrar.

--- E-Agenda.Dominio/ModuloCompromissos/Compromisso.cs
+++ E-Agenda.Dominio/ModuloCompromissos/Compromisso.cs
@@ -5,34 +5,37 @@ namespace E_Agenda.Dominio.ModuloCompromissos;
 public class Compromisso : EntidadeBase<Compromisso>
 {
     public string Assunto { get; set; }
-    public DateOnly DataOcorrencia { get; set; }
+    public DateOnly Data { get; set; }
     public TimeOnly HoraInicio { get; set; }
     public TimeOnly HoraTermino { get; set; }
     public TipoCompromisso Tipo { get; set; }
-    public string LocalOuLink { get; set; }
+    public string Local { get; set; }
+    public string Link { get; set; }
     public Contato? Contato { get; set; } = null;
 
     public Compromisso() { }
-    public Compromisso(string assunto, DateOnly dataOcorrencia, TimeOnly inicio, TimeOnly termino, bool ehRemoto, string localOuLink, Contato? contato) : this()
+    public Compromisso(string assunto, DateOnly dataOcorrencia, TimeOnly inicio, TimeOnly termino, bool ehRemoto, string local, string link, Contato? contato) : this()
     {
         Id = Guid.NewGuid();
         Assunto = assunto;
-        DataOcorrencia = dataOcorrencia;
+        Data = dataOcorrencia;
         HoraInicio = inicio;
         HoraTermino = termino;
         Tipo = ehRemoto ? TipoCompromisso.Remoto : TipoCompromisso.Presencial;
-        LocalOuLink = localOuLink;
+        Local = local;
+        Link = link;
         Contato = contato;
     }
 
     public override void Atualizar(Compromisso registroEditado)
     {
         Assunto = registroEditado.Assunto;
-        DataOcorrencia = registroEditado.DataOcorrencia;
+        Data = registroEditado.Data;
         HoraInicio = registroEditado.HoraInicio;
         HoraTermino = registroEditado.HoraTermino;
         Tipo = registroEditado.Tipo;
-        LocalOuLink = registroEditado.LocalOuLink;
+        Local = registroEditado.Local;
+        Link = registroEditado.Link;
         Contato = registroEditado.Contato;
     }
 }
